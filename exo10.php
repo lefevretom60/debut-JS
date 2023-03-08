bd8f89f659e5c8461372835d493a9a8b349c9347
lefevretom60 <113522471+lefevretom60@users.noreply.github.com>
2023-03-08 17:41:30 +0100

    Create exo10.php

---
+++ exo10.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/exercice10.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercice 10 en JS</h1>
+    <div class="divA">
+        <div id="MaDivNum1" class="divD">
+            coucou
+        </div>
+        <div id="Methode1" class="divD">
+            Appuie-moi dessus !
+        </div>
+        <div id="Methode2" class="divD">
+            Passe-moi dessus !
+        </div>
+        <div id="Methode3" class="divD" tabindex="0">
+            Frappe ton clavier
+        </div>
+    </div>
+   <!--<h3> Première question</h3>
+    <div class="divA">
+        <div id="Methode4" class="class1">
+            Cette div changera de couleur si tu appuies dessus !
+        </div>
+    </div>
+-->
+    <h3> Deuxième question</h3>
+    <div class="divA">
+        <div id="Methode5" class="class1">
+            disparition
+        </div>
+    </div>
+
+</body>
+<script src="main10.js"></script>
+</html>  
